0eee103ffa99069b6654a5441f6619c774c93ca6
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2024-03-01 22:46:53 +0530

    Update RegisterPage.jsx

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -8,7 +8,7 @@ function RegisterPage() {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch('https://ecommerce-boilerplate-kappa.vercel.app/users', {
+      const response = await fetch('https://localhost:3000/users', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
