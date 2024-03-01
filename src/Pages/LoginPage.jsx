4bbf02c1f2159a709dae8eab5318a38b149fa459
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2024-03-01 22:46:13 +0530

    Update LoginPage.jsx

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -11,7 +11,7 @@ function LoginPage() {
   useEffect(() => {
     const authenticateUser = async () => {
       try {
-        const response = await fetch('https://ecommerce-boilerplate-kappa.vercel.app/users', {
+        const response = await fetch('https://localhost:3000/users', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
