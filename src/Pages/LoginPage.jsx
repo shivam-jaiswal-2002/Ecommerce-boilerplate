28484628dcc0a15241bb4c9819575ead1f76b8f0
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2024-04-05 12:20:05 +0530

    Update LoginPage.jsx

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -11,7 +11,7 @@ function LoginPage() {
   useEffect(() => {
     const authenticateUser = async () => {
       try {
-        const response = await fetch('https://localhost:3000/users', {
+        const response = await fetch('http://localhost:3000/users', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
