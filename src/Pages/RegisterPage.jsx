2146e76d80e9f35af3db79a4e02e0f8c2fbd3e77
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2024-04-05 12:19:00 +0530

    Update RegisterPage.jsx

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -8,7 +8,7 @@ function RegisterPage() {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch('https://localhost:3000/users', {
+      const response = await fetch('http://localhost:3000/users', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
